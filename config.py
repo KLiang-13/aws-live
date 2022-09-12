b202eb4ac0030d3ed39c03d13cc6eae80274b608
KLiang-13 <87765044+KLiang-13@users.noreply.github.com>
2022-09-13 00:53:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cdlp4u0kosbj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "angkuanliang-employee"
+customregion = "us-east-1"
